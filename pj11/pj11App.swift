0404e03c12e37839ff85d036d0a5c5c015cd29a9
mirmikov <mirmikovmisa@gmail.com>
2024-10-10 00:11:55 +0300

    Initial Commit

---
+++ pj11/pj11App.swift
@@ -0,0 +1,17 @@
+//
+//  pj11App.swift
+//  pj11
+//
+//  Created by Michael Mirmikov on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct pj11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
